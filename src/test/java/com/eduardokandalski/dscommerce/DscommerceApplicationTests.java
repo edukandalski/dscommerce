9495664c753e345f8c7b28c5f8eab42a534368a6
Eduardo Kandalski <edukandalski@gmail.com>
2025-03-08 20:07:36 -0300

    Primeiro commit - Spring Initializr boilerplate

---
+++ src/test/java/com/eduardokandalski/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardokandalski.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
