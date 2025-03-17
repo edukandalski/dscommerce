8da7e47c7698c4da68d200631e324f338ddf73b4
Eduardo Kandalski <edukandalski@gmail.com>
2025-03-16 21:07:47 -0300

    Adicionado endpoint para getUserLogged

---
+++ src/main/java/com/eduardokandalski/dscommerce/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.eduardokandalski.dscommerce.controllers;
+
+import com.eduardokandalski.dscommerce.dto.UserDTO;
+import com.eduardokandalski.dscommerce.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users" )
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+        UserDTO dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+}
